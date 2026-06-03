6991f32285abad0f5ae4177975f7b1c97aa642c0
Biggie <code@bigo.lu>
2026-06-02 21:12:40 -0400

    Include flake inputs in GC root

---
+++ nix/outputs/devshells/ci.nix
@@ -0,0 +1,24 @@
+{ perSystem, inputs, ... }:
+perSystem.devshell.mkShell (
+  { extraModulesPath, pkgs, ... }:
+  {
+    imports = [
+      "${extraModulesPath}/locale.nix"
+    ]
+    ++ (with inputs.devshell-modules.devshellModules; [
+      minimal
+      state
+      gcRoot
+    ]);
+
+    gcRoot = {
+      roots.flake.inputs = inputs;
+      diff.enable = false;
+    };
+
+    devshell.packages = [perSystem.self.default] ++ (with pkgs; [
+      # For the `run` steps in CI workflows
+      bash
+    ]);
+  }
+)
